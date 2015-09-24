f322c96aee4d3e50036d4d3ced43292b7e7179f6
lbdroid <definesinsanity@gmail.com>
2015-09-23 22:14:21 -0400

    Kill the process to end a lingering wakelock
    Problem is that when you disable the service, it doesn't properly die. Whatever wakelock was started by SystemUpdateService persists as long as com.google.android.gms process is running. So we kill the process precisely once.
    
    The RootShell library acts funny when you are entering multiple commands. For whatever reason, they are not all executed, and not in actual order, and there is no way to sleep it between them because its all sharing a single process. So we do the first step of disabling the component com.google.android.gms/.updater.SystemUpdateService, and then create an alarm for 20 seconds to launch the OnAlarmReceiver, which checks in the output of "dumpsys power" for the SystemUpdateService wakelock, and if it finds it, kills the com.google.android.gms process.

---
+++ src/ml/rabidbeaver/stopota/OnAlarmReceiver.java
@@ -0,0 +1,53 @@
+package ml.rabidbeaver.stopota;
+
+import com.stericson.RootShell.RootShell;
+import com.stericson.RootShell.execution.Command;
+import com.stericson.RootShell.execution.Shell;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class OnAlarmReceiver extends BroadcastReceiver {
+
+	String logtag = "StopOTA-Alarm";
+	private Shell rshell;
+	private String pid="";
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.d(logtag,"Running onReceive");
+		if (RootShell.isAccessGiven()){
+			try {
+				rshell = RootShell.getShell(true);
+			} catch (Exception e1) {}
+			Log.d(logtag,"Root access is granted");
+			
+			Command command = new Command(0,"dumpsys power | grep SystemUpdateService"){
+				@Override
+				public void commandOutput(int id, String line){
+					Log.d(logtag,line);
+					if (line.contains("SystemUpdateService")){
+						String s1 = line.substring(line.indexOf("pid="));
+						pid = s1.substring(s1.indexOf("=")+1,s1.indexOf(","));
+						Command killcmd = new Command(0,"kill -9 "+pid);
+						try {
+							Log.d(logtag,"Killing com.google.android.gms to clear SystemUpdateService wakelock");
+							rshell.add(killcmd);
+						} catch (Exception e) {}
+					}
+				}
+			};
+
+			try {
+				rshell.add(command);
+			} catch (Exception e) {
+				Log.d(logtag,"exception");
+			}
+
+		} else {
+			Log.d(logtag,"Root access not granted");
+		}
+	}
+}
--- src/ml/rabidbeaver/stopota/OnBootReceiver.java
+++ src/ml/rabidbeaver/stopota/OnBootReceiver.java
@@ -2,37 +2,57 @@ package ml.rabidbeaver.stopota;
 
 import com.stericson.RootShell.RootShell;
 import com.stericson.RootShell.execution.Command;
+import com.stericson.RootShell.execution.Shell;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.util.Log;
 
 public class OnBootReceiver extends BroadcastReceiver {
+	
+	private Shell rshell;
+	String logtag = "StopOTA-Boot";
+	boolean success = false;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Log.d("StopOTA","Running onReceive");
+		Log.d(logtag,"Running onReceive");
 		if (RootShell.isAccessGiven()){
-			Log.d("StopOTA","Root access is granted");
-			Command command = new Command(0,
+			
+			Log.d(logtag,"Root access is granted");
+			
+			final Command command1 = new Command(0,
 					"pm disable com.google.android.gms/.update.SystemUpdateService",
-					"pm disable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver",
-					"pm disable com.google.android.gms/.update.SystemUpdateService$Receiver",
-					"pm disable com.google.android.gms/.update.SystemUpdateService$OtaPolicyReceiver",
 					"rm /data/data/com.google.android.gms/shared_prefs/update.xml",
 					"rm /data/data/com.google.android.gms/app_download/*"){
 				@Override
 				public void commandOutput(int id, String line){
-					Log.d("StopOTA",line);
+					Log.d(logtag,line);
 				}
 			};
+			
 			try {
-				RootShell.getShell(true).add(command);
-			} catch (Exception e) {}
+				rshell = RootShell.getShell(true);
+				rshell.add(command1);
+				Log.d(logtag,"Components disabled.");
+				success=true;
+			} catch (Exception e) {
+				Log.d(logtag,"exception");
+			}
+
 		} else {
-			Log.d("StopOTA","Root access not granted");
+			Log.d(logtag,"Root access not granted");
 		}
-		Log.d("StopOTA","complete");
+		if (success){
+			Log.d(logtag,"Complete, setting kill alarm for 20 seconds");
+			AlarmManager mgr=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+			Intent i=new Intent(context, OnAlarmReceiver.class);
+			PendingIntent pi=PendingIntent.getBroadcast(context, 0, i, 0);
+			mgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime()+20000, pi);
+		} else Log.d(logtag,"Unsuccessful");
 	}
 }
